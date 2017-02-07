9c1b10910dadc2941c6758df683001c93f811f39
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2017-02-07 00:25:51 -0800

    working with namespaces in csharp

---
+++ Basics/AssembliesandNameSpaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesandNameSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "We want to write in this file";
+            File.WriteAllText(@"C:\text\myfile.txt", text);
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+            File.WriteAllText(@"c:\text\myfile.txt", reply);
+
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
