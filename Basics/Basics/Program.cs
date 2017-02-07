11d2e5c258b95be2e1b733ea1e5bd01b9b284506
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2017-02-06 21:01:24 -0800

    create a new class

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -10,6 +10,15 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
         }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
     }
 }
