5ce5f791f9272cfeee23e332248574833be0eb2e
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2017-02-10 05:17:11 -0800

    create a reusable class and use that in another project

---
+++ MyClient/MyClassLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class DownloadFile
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebPAge(url);
+        }
+        public string ScrapeWebpage(string url, string filePath)
+        {
+            string reply = GetWebPAge(url);
+            File.WriteAllText(filePath, reply);
+            return reply;
+        }
+        private string GetWebPAge(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DownloadFile dlFile = new DownloadFile();
+            string value = dlFile.ScrapeWebpage("http://www.raziyeh.info");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
