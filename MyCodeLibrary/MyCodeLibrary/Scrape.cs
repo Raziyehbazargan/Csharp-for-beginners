9a9f22ee05d2f90989eb487222aca606a3c31e23
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2017-02-10 04:58:27 -0800

    create new class

---
+++ MyCodeLibrary/MyCodeLibrary/Scrape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCodeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebPage(url);
+
+        }
+
+        public string ScrapeWebpage(string url, string filePath)
+        {
+            string reply = GetWebPage(url);
+
+            File.WriteAllText(filePath, reply);
+            return reply;
+
+        }
+        public string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
